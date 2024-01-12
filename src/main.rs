810875639fd44f62f214e8b2dbf6376609bd6215
JulienH2000 <julien.hugues@outlook.com>
2024-01-12 01:02:45 +0100

    add some input security

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,35 @@ struct Resistor {
     tolerance: f32,
 }
 
-fn colors_to_values(input: String) -> Resistor {
-    let split_vec: Vec<&str> = input.split(' ').collect();
+fn colors_to_values(input: String) -> Result<Resistor, &'static str> {
+    let split_vec: Result<Vec<&str>, &str> = input.split_whitespace().map(Ok).collect();
     let mut v: Vec<u8> = Vec::new();
-    for i in 0..=split_vec.len()-1 {
-        match split_vec[i].to_lowercase().as_str() {
-            "black" => v.push(0),
-            "brown" => v.push(1),
-            "red" => v.push(2),
-            "orange" => v.push(3),
-            "yellow" => v.push(4),
-            "green" => v.push(5),
-            "blue" => v.push(6),
-            "violet" => v.push(7),
-            "grey" => v.push(8),
-            "white" => v.push(9),
-            "gold" => v.push(10),
-            "silver" => v.push(11),
-            _ => v.push(0),
-        }
+    match split_vec{
+        Ok(color) => 
+        if color.len() < 4 {
+            return Err("Not 4/5 ring color sequence");
+        } else {
+        for i in 0..=color.len()-1 {
+            match color[i].to_lowercase().as_str() {
+                "black" => v.push(0),
+                "brown" => v.push(1),
+                "red" => v.push(2),
+                "orange" => v.push(3),
+                "yellow" => v.push(4),
+                "green" => v.push(5),
+                "blue" => v.push(6),
+                "violet" => v.push(7),
+                "grey" => v.push(8),
+                "white" => v.push(9),
+                "gold" => v.push(10),
+                "silver" => v.push(11),
+                _ => return Err("Invalid color"),
+            }
+        }},
+        Err(error) => return Err(error),
+
     }
+    
 
     let mut output = Resistor {
         first_digit: v[0],
@@ -39,16 +48,16 @@ fn colors_to_values(input: String) -> Resistor {
     };
     output.mult = 10_u8.pow(output.mult as u32);
     output.tolerance = color_to_tolerance(output.tolerance);
-    output
+    Ok(output)
 }
 
 fn color_to_tolerance(i:f32) -> f32 {
     let t = match i {
-        2.0 => 1.0,
-        3.0 => 2.0,
-        5.0 => 0.5,
-        6.0 => 0.25,
-        7.0 => 0.1,
+        2_f32 => 1.0,
+        3_f32 => 2.0,
+        5_f32 => 0.5,
+        6_f32 => 0.25,
+        7_f32 => 0.1,
         _ => 0.0,
     };
     t
@@ -72,15 +81,22 @@ fn format_resistor(r: f32) -> (f32, char) {
 } */
 
 fn main() {
+
+    loop {
     let mut user_input = String::new();
-
-
     io::stdin()
         .read_line(&mut user_input)
         .expect("failed");
 
     let resistor = colors_to_values(user_input);
     
-    println!("{}", calc_resistor(resistor));
+    match resistor {
+        Ok(r) => println!("{}", calc_resistor(r)),
+        Err(e) => {
+            println!("{}", e);
+            continue},
+    }
+    
+    }
 
 }
