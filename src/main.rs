3df6ac928e7f8aae82ef9e6fa339c588135071df
JulienH2000 <julien.hugues@outlook.com>
2024-01-16 23:10:48 +0100

    new branch, rewrite using impl

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,219 @@
 use std::collections::HashMap;
 use std::io;
-//use std::error;
+use std::fmt::Error;
 
 struct Resistor {
-    first_digit: Option<u8>,
-    sec_digit: Option<u8>,
-    third_digit: Option<u8>,
+    first_digit: Option<f32>,
+    sec_digit: Option<f32>,
+    third_digit: Option<f32>,
     mult: Option<f32>,
     tolerance: Option<f32>,
 }
+
+impl Resistor {
+    fn as_array (&self) -> [&Option<f32>; 5] {
+        [&self.first_digit,
+        &self.sec_digit,
+        &self.third_digit,
+        &self.mult,
+        &self.tolerance]
+    }
+
+    fn set_mult (self) -> Result<Resistor, &'static str> {
+        let m = match self.mult {
+            None => None,
+            Some(m) => if m <= 7_f32{
+                        Some(
+                        (10_u32
+                        .checked_pow(m as u32)
+                        .expect("overflow")) as f32)
+                    } else if m == 10_f32 {
+                        Some(0.1)
+                    } else if m == 11_f32 {
+                        Some(0.01)
+                    } else {
+                        None
+                    },
+        };
+
+        Ok(Resistor {
+            mult: m,
+            ..self          
+            }
+        )
+    }
+
+    fn set_tolerance (self) -> Result<Resistor, &'static str> {
+        let t = match self.tolerance {
+            None => None,
+            Some(c) => Some(match c as i32 {
+                1 => 1.0,
+                2 => 2.0,
+                5 => 0.5,
+                6 => 0.25,
+                7 => 0.1,
+                10 => 5_f32,
+                11 => 10_f32,
+                _ => 0.0,
+            },)
+        };
+        Ok(Resistor {
+            tolerance: t,
+            ..self          
+            }
+        )
+
+    }
+}
+
+struct Colors {
+    first_color: Option<String>,
+    sec_color: Option<String>,
+    third_color: Option<String>,
+    mult: Option<String>,
+    tolerance: Option<String>,
+}
+
+impl Colors {
+    fn as_array (&self) -> [&Option<String>; 5] {
+        [&self.first_color,
+        &self.sec_color,
+        &self.third_color,
+        &self.mult,
+        &self.tolerance]
+    }
+
+    pub fn to_values (&self) -> Result<Resistor, &'static str>{
+        let output = Resistor { first_digit: None, sec_digit: None, third_digit: None, mult: None, tolerance: None};
+        for i in 0..self.as_array().len() {
+            match self.as_array()[i] {
+                Some(v) => match v.to_lowercase().as_str() {
+                                "black" => output.as_array()[i] = &Some(0_f32),
+                                "brown" => output.as_array()[i] = &Some(1_f32),
+                                "red" => output.as_array()[i] = &Some(2_f32),
+                                "orange" => output.as_array()[i] = &Some(3_f32),
+                                "yellow" => output.as_array()[i]  = &Some(4_f32),
+                                "green" => output.as_array()[i] = &Some(5_f32),
+                                "blue" => output.as_array()[i] = &Some(6_f32),
+                                "violet" => output.as_array()[i] = &Some(7_f32),
+                                "grey" => output.as_array()[i] = &Some(8_f32),
+                                "white" => output.as_array()[i] = &Some(9_f32),
+                                "gold" => output.as_array()[i] = &Some(10_f32),
+                                "silver" => output.as_array()[i] = &Some(11_f32),
+                                _ => return Err("Invalid color !!"),
+                },
+                None => return Err("Invalid Colors Struct !!"),
+            }
+        }
+        println!("{:?}", output.as_array());
+        Ok(output)
+
+        
+    }
+}
+
+
+fn string_to_color (input: String) -> Result<Colors, &'static str> {
+    let v: Vec<Option<String>> = input.split_whitespace().map(|s| Some(s.to_string())).collect();
+    println!("{:?}", v);
+    if v.len() == 4 {
+        Ok(Colors {
+            first_color: v[0].clone(),
+            sec_color: v[1].clone(),
+            third_color: None,
+            mult: v[2].clone(),
+            tolerance: v[3].clone(),
+        })
+    } else if  v.len () ==  5 {
+        Ok(Colors {
+            first_color: v[0].clone(),
+            sec_color: v[1].clone(),
+            third_color: v[2].clone(),
+            mult: v[3].clone(),
+            tolerance: v[4].clone(),
+        })
+    } else {
+        return Err("Not 4/5 rings color sequence !!");
+    }
+    
+}
+
+fn calc_resistor(i: Resistor) -> String {
+    let mut values= HashMap::new();
+    values.insert("first_digit", i.first_digit.unwrap_or(0.) as f64);
+    values.insert("sec_digit", i.sec_digit.unwrap_or(0.) as f64);
+    values.insert("third_digit", i.third_digit.unwrap_or(0.) as f64);
+    values.insert("mult", i.mult.unwrap_or(0_f32) as f64);
+    values.insert("tolerance", i.tolerance.unwrap_or(0.0_f32) as f64);
+
+    let r:f64;
+    if None == i.third_digit {
+        r= (
+            values.get("first_digit").unwrap() *10_f64
+            +
+            values.get("sec_digit").unwrap() *1_f64
+            ) *
+            values.get("mult").unwrap();
+    } else {
+        r= (
+            values.get("first_digit").unwrap() *100_f64
+            +
+            values.get("sec_digit").unwrap() *10_f64
+            +
+            values.get("third_digit").unwrap() *1_f64
+            ) *
+            values.get("mult").unwrap_or(&1_f64);
+    }
+    
+    let result = match format_resistor(r) {
+        Ok(r) => r,
+        Err(_) => panic!("formatting failed !!")
+    };
+    let s:String = format!("{:.1} {} +/-{}%", result.0, result.1, values.get("tolerance").unwrap());
+    s            
+}
+
+fn format_resistor(r: f64) -> Result<(f64, String), &'static str> {
+    let output:(f64, String);
+    let rounded = r as i32;
+    let len = rounded.to_string();
+    if len.len() <= 3 {
+        output = (r, String::from("Ohms"));
+    } else if len.len() <= 6 {
+        output = (r/1000_f64, String::from("kOhms"));
+    } else if len.len() <= 9 {
+        output = (r/100000_f64, String::from("MOhms"));
+    } else {
+        output = (r, String::from("Ohms"));
+    }
+    Ok(output)
+}
+
+fn main () {
+
+    loop {
+    let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("failed");
+
+    let colors_input;
+    match string_to_color(user_input) {
+        Ok(r) => colors_input = r,
+        Err(_) => panic!("failed to string to colors !!"),
+    }
+    let resistor;
+      match colors_input.to_values() {
+        Ok(v) => resistor = v,
+        Err(_) => panic!("failed to colors to values !!"),
+    }
+
+    println!("{}",calc_resistor(resistor.set_mult().unwrap().set_tolerance().unwrap()));
+}
+}
+
+// old code
+/*
 
 fn colors_to_values(input: String) -> Result<Resistor, &'static str> {
     let split_vec: Result<Vec<&str>, &str> = input.split_whitespace().map(Ok).collect();
@@ -183,6 +388,10 @@ fn main() {
     }
 
 }
+
+
+*/
+
 
 fn banner() {
     println!("
